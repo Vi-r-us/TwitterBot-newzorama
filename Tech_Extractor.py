f746a436c8e34c4a9e292ee1443b116be0f4ebde
Vi-r-us <sahil23012000@gmail.com>
2021-04-15 14:55:28 +0530

    true errors fixed

--- Tech_Extractor.py
+++ Tech_Extractor.py
@@ -10,7 +10,7 @@ final = []
 
 # get the date of the day , last hour from current time date
 last_hour_date_time = datetime.datetime.now() + datetime.timedelta(hours=4, minutes=30)
-date = str(last_hour_date_time).split()[0]              # return as a string format: yy-mm-dd
+date = str(last_hour_date_time.strftime("%#d %B %Y"))            # return as a string format: dd-mm-yy eg:- 7 April 2021
 
 
 # This Function return optimal news
@@ -74,7 +74,7 @@ def give_me_news():
             temp = page_soup.find('div', class_="content_text row description").find_all('p')
             content = f"{temp[0].text} {temp[1].text} {temp[2].text}"
 
-            last_hour_date_time = datetime.datetime.now() - datetime.timedelta(hours=4, minutes=30)
+            last_hour_date_time = datetime.datetime.now() + datetime.timedelta(hours=4, minutes=30)
 
             # if the news is from last hour then only append it to final
             if page_soup.find('div', class_="content_block white_bg row margin_b30").find('div', class_="dateline").span['title'].split(" ")[4].split(":")[0] == last_hour_date_time.strftime("%H"):
